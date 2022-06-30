05250ec46fd91fdec5c351b2272b53064828eb7e
mara-zt <malehnovich@gmail.com>
2022-06-30 16:30:38 +0300

    add adaptability

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ $(function ()
     $('.preheader__close').on('click', function () {
        $('.preheader').toggleClass('preheader--haden')
     })
+    $('.header__mobile__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--activ')
+     })
+
 }); 
