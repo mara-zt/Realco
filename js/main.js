fae626744f36d1ef3c9de95240eecbb1b2dadf93
mara-zt <malehnovich@gmail.com>
2022-07-02 09:50:50 +0300

    update adaptive

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ $(function ()
     })
     $('.header__mobile__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--activ')
-     })
+    })
+    $('.Pages').on('click', function () {
+      $('.Pages--ul').toggleClass('Pages--ul--activ')
+    })
+    $('.CMS_Pages').on('click', function () {
+      $('.CMS_Pages--ul').toggleClass('CMS_Pages--ul--activ')
+    })
+    $('.Utility_Pages').on('click', function () {
+      $('.Utility_Pages--ul').toggleClass('Utility_Pages--ul--activ')
+   })
 
+   
 }); 
